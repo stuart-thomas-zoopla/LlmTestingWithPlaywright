512beb35558022a78bdf0249f849dfa51a386fff
Stuart Thomas <77386766+stuart-thomas-zoopla@users.noreply.github.com>
2024-03-26 17:18:11 +0000

    Formatting and naming

--- helpers/evaluatorLlm.ts
+++ helpers/evaluatorLlm.ts
@@ -3,12 +3,13 @@ const singleGenerate = '/api/generate'
 const model = 'llama2'
 const stream = false
 
-async function validateReponseWithEvaluator(request, response, expectedWords) {
+async function validateReponseWithEvaluator(request, response, expectedResponse) {
     const originalResponse = JSON.stringify(response.response)
     const evaluatorResponse = await request.post(singleGenerate, {
         data: {
             model: model,
-            prompt: "If the following 2 statements appear to give accurate and corroborating answers to the same question respond 'Yes, the statements are similar'. Statement 1 - " + originalResponse + " - end of statement 1. Statement 2 - " + expectedWords + " - end of statement2",
+            prompt: "If the following 2 statements appear to give accurate and corroborating answers to the same question respond \
+            'Yes, the statements are similar'. Statement 1 - " + originalResponse + " - end of statement 1. Statement 2 - " + expectedResponse + " - end of statement2",
             stream: stream,
         }
     });
